3a8ff2bb26c86cd57cccfe82722d393636f928ab
RomjanHossain <romjanhossain726526@gmail.com>
2020-04-19 15:36:34 +0600

    basic ruls completed

--- game.js
+++ game.js
@@ -8,85 +8,109 @@ GAME RULES:
 */
 
 /// setting varibles for game
-let scores, roundScore, activePlayer, dice;
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-
-//setting img display to none
-
+let scores, roundScore, activePlayer, dice, gameOn;
 let imgDice = document.querySelector('.dice');
-imgDice.style.display = 'none';
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    gameOn = true;
+
+    //setting img display to none
+
+    imgDice.style.display = 'none';
+    console.log('skjdf');
+
+    /// change score p1 p2
+
+    // setting score to 0
+    let currentScoreP1 = document.getElementById('current-0').textContent = '0';
+    let currentScoreP2 = document.getElementById('current-1').textContent = '0';
+    //seting score to 0
+    let ScoreP0 = document.getElementById('score-0').textContent = '0';
+    let ScoreP1 = document.getElementById('score-1').textContent = '0';
+    // resign thire name
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
+    //rid of 2nd palyer active and get 1st player active
+    document.querySelector('.player-0-panel').classList.remove('Winner');
+    document.querySelector('.player-1-panel').classList.remove('Winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+}
+
+init();
 
 
-/// change score p1 p2
-let currentScoreP1 = document.getElementById('current-0').textContent = '0';
-let currentScoreP2 = document.getElementById('current-1').textContent = '0';
-let ScoreP0 = document.getElementById('score-0').textContent = '0';
-let ScoreP1 = document.getElementById('score-1').textContent = '0';
 
 //swapPlayer function//
 const swapPlayer = () => {
-    imgDice.style.display = 'none';
+    if (activePlayer === 1) {
+        activePlayer = 0;
+    } else if (activePlayer == 0) {
+        activePlayer = 1;
+    }
+    // imgDice.style.display = 'none';
     roundScore = 0;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    //changing their score
+    document.querySelector('#current-0').textContent = 0;
+    document.querySelector('#current-1').textContent = 0;
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
-    // activebtn.classList.remove('active');
 
-    if (activePlayer === 1) {
-      activePlayer = 0;
-    } else if (activePlayer == 0) {
-      activePlayer = 1;
+};
+
+// Who won??
+const winGame = () => {
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        imgDice.style.display = 'none';
+        gameOn = false;
+
+    } else {
+        swapPlayer();
     }
+};
 
-  };
 
 //////////dice roll method
 let diceRoll = document.querySelector('.btn-roll').addEventListener('click', () => {
-    dice = Math.floor(Math.random() * 6) + 1;
-    console.log(dice);
-    imgDice.style.display = 'block';
+    if (gameOn) {
+        dice = Math.floor(Math.random() * 6) + 1;
+        // console.log(dice);
+        imgDice.style.display = 'block';
 
-    //this all if else statement can be done as simple method
-    imgDice.src = 'img/dice-' + dice + '.png';
-    // if (dice === 1) {
-    //   imgDice.src = 'img/dice-1.png';
-    // } else if (dice === 2) {
-    //   imgDice.src = 'img/dice-2.png';
-    // } else if (dice === 3) {
-    //   imgDice.src = 'img/dice-3.png';
-    // } else if (dice === 4) {
-    //   imgDice.src = 'img/dice-4.png';
-    // } else if (dice === 5) {
-    //   imgDice.src = 'img/dice-5.png';
-    // } else if (dice === 6) {
-    //   imgDice.src = 'img/dice-6.png';
-    // } else {
-    //   imgDice.style.display = 'none';
-    // }
-
-    if (dice !== 1) {
-      //add score
-      roundScore += dice;
-      // scores[activePlayer] += dice;
-      // console.log(roundScore);
-      document.querySelector('#current-' + activePlayer).textContent = roundScore;
-
-    } else {
-      swapPlayer();
-
+        imgDice.src = 'img/dice-' + dice + '.png';
+        if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            swapPlayer();
+        }
     }
-  });
+
+});
 
 
 // btn hold//
 let btnhold = document.querySelector('.btn-hold').addEventListener('click', () => {
-    // console.log('ho');
-    scores[activePlayer] += roundScore;
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    if (gameOn) {
+        scores[activePlayer] += roundScore;
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        winGame();
+        // swapPlayer();
+    }
 
-    swapPlayer();
-  });
-// score of players
+});
+
+let btnNew = document.querySelector('.btn-new').addEventListener('click', init);
+
+
+/// Creating some new ruls for the game!
