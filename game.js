99678f392816c44147f198a5da165955d97cb59a
RomjanHossain <romjanhossain726526@gmail.com>
2020-04-19 17:02:41 +0600

    new rules completed

--- game.js
+++ game.js
@@ -9,37 +9,43 @@ GAME RULES:
 
 /// setting varibles for game
 let scores, roundScore, activePlayer, dice, gameOn;
-let imgDice = document.querySelector('.dice');
+// let imgDice = document.querySelector('.dice');
+
+let DICE = document.getElementById('dice-1');
+let DICE2 = document.getElementById('dice-2');
+DICE.style.display = 'block';
+DICE2.style.display = 'block';
 
 function init() {
-    scores = [0, 0];
-    roundScore = 0;
-    activePlayer = 0;
-    gameOn = true;
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gameOn = true;
 
-    //setting img display to none
+  //setting img display to none
 
-    imgDice.style.display = 'none';
-    console.log('skjdf');
+  DICE.style.display = 'none';
+  DICE2.style.display = 'none';
+  // console.log('skjdf');
 
-    /// change score p1 p2
+  /// change score p1 p2
 
-    // setting score to 0
-    let currentScoreP1 = document.getElementById('current-0').textContent = '0';
-    let currentScoreP2 = document.getElementById('current-1').textContent = '0';
-    //seting score to 0
-    let ScoreP0 = document.getElementById('score-0').textContent = '0';
-    let ScoreP1 = document.getElementById('score-1').textContent = '0';
-    // resign thire name
-    document.querySelector('#name-0').textContent = 'Player 1';
-    document.querySelector('#name-1').textContent = 'Player 2';
-    //rid of 2nd palyer active and get 1st player active
-    document.querySelector('.player-0-panel').classList.remove('Winner');
-    document.querySelector('.player-1-panel').classList.remove('Winner');
-    document.querySelector('.player-0-panel').classList.remove('active');
-    document.querySelector('.player-1-panel').classList.remove('active');
-    document.querySelector('.player-0-panel').classList.add('active');
-    document.querySelector('.player-1-panel').classList.remove('active');
+  // setting score to 0
+  let currentScoreP1 = document.getElementById('current-0').textContent = '0';
+  let currentScoreP2 = document.getElementById('current-1').textContent = '0';
+  //seting score to 0
+  let ScoreP0 = document.getElementById('score-0').textContent = '0';
+  let ScoreP1 = document.getElementById('score-1').textContent = '0';
+  // resign thire name
+  document.querySelector('#name-0').textContent = 'Player 1';
+  document.querySelector('#name-1').textContent = 'Player 2';
+  //rid of 2nd palyer active and get 1st player active
+  document.querySelector('.player-0-panel').classList.remove('Winner');
+  document.querySelector('.player-1-panel').classList.remove('Winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
 }
 
 init();
@@ -49,9 +55,9 @@ init();
 //swapPlayer function//
 const swapPlayer = () => {
     if (activePlayer === 1) {
-        activePlayer = 0;
+      activePlayer = 0;
     } else if (activePlayer == 0) {
-        activePlayer = 1;
+      activePlayer = 1;
     }
     // imgDice.style.display = 'none';
     roundScore = 0;
@@ -63,33 +69,55 @@ const swapPlayer = () => {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-};
+  };
 
 // Who won??
 const winGame = () => {
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-        imgDice.style.display = 'none';
-        gameOn = false;
+    let input = document.querySelector('.final-score').value;
+    let winningScore;
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
+    if (scores[activePlayer] >= winningScore) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      DICE.style.display = 'none';
+      DICE2.style.display = 'none';
+      gameOn = false;
 
     } else {
-        swapPlayer();
+      swapPlayer();
     }
-};
+  };
 
-
+let dicePrev, dice2;
 //////////dice roll method
 let diceRoll = document.querySelector('.btn-roll').addEventListener('click', () => {
     if (gameOn) {
         dice = Math.floor(Math.random() * 6) + 1;
+        dice1 = Math.floor(Math.random() * 6) + 1;
         // console.log(dice);
-        imgDice.style.display = 'block';
+        dicePrev = dice;
+        // console.log('this is dicePrev ' + dicePrev);
+        // console.log('this is dice2 ' + dice2);
+        if (dice1 === dice) {
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+            swapPlayer();
+        }
 
-        imgDice.src = 'img/dice-' + dice + '.png';
-        if (dice !== 1) {
-            roundScore += dice;
+        //
+        DICE.style.display = 'block';
+        DICE2.style.display = 'block';
+
+        DICE.src = 'img/dice-' + dice + '.png';
+        DICE2.src = 'img/dice-' + dice1 + '.png';
+        if (dice !== 1 && dice1 !== 1) {
+            roundScore += dice + dice1;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
             swapPlayer();
@@ -114,3 +142,8 @@ let btnNew = document.querySelector('.btn-new').addEventListener('click', init);
 
 
 /// Creating some new ruls for the game!
+/*
+rule -1: if a player rolls two 6 then it's next players turn
+  --rule-1 DONE
+issu -1: create a input field where user can set the winng score
+*/
